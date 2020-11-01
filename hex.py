c801b6e521ca4a2cc8360a22ca35497092cf3fbe
darkplaguemaster <andrewlinj1@gmail.com>
2020-11-01 18:22:57 -0500

    completed XOR part

--- hex.py
+++ hex.py
@@ -1,26 +1,24 @@
 import sys
 
+mode = sys.argv[1]
+keyfile = sys.argv[2]
+inpfile = sys.argv[3]
+with open (keyfile) as fp:
+	key = fp.read()[:-1]
+with open (inpfile) as fp:
+	inp = fp.read()[:-1]
+debug = False
+
+if (debug):
+	print("mode:"+ mode)
+	print("key: "+ key)
+	print("inp: "+ inp)
+
+counter = 0
+output = []
+while (counter < len(inp)):
+	output.append(ord(inp[counter]) ^ ord(key[counter % len(key)]))
+	counter += 1
+print(output)
 
 
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-	mode = sys.argv[1]
-	keyfile = sys.argv[2]
-	inpfile = sus.argv[3]
-	with open (keyfile) as fp:
-		key = fp.read()[:-1]
-	with open (inpfile) as fp:
-		key = fp.read()[:-1]
-	debug = False
-
-	if (debug):
-		print("mode:"+mode)
-		print("key: "+key)
-		print("inp: "+inp)
